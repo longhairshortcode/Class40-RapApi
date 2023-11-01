10aa9c8f5f2b72af47e78be73dd4d0bfb1c54156
Rona <longhairshortcode@gmail.com>
2023-10-31 17:44:12 -0700

    adding to connecting to db

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 // all the consts & requirements
 const express = require('express')
+const MongoClient = require('mongodb').MongoClient
 const app = express()
 const PORT = 8080
 
 
 //connection to database
+let db,
+    dbConnectionStr = process.env.DB_STRING
+    dbName = "RapperNamesDB"
 
-
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })    
 //all the app.use & app.set
 app.use(express.json())
 app.use(express)
 
 
 //ROUTE : app.get('/'...) + .catch(error => console.error(error))
-
+app.get('/', (req, res)=>{
+    db.collection('rappers').find().sort({likes: -1}).toArray()
+    .then(data => {
+        respond.render('index.ejs', {info: data})
+    })
+    .catch(error => console.error(error))
+})
 
 
 
