a723e86af2fdbd390a608d9c2ecc40e7a45f170b
Rona <longhairshortcode@gmail.com>
2023-11-04 23:27:03 -0700

    edits

--- server.js
+++ server.js
@@ -86,10 +86,6 @@ app.listen(process.env.PORT || PORT, () => {
     console.log(`Sever is running on port ${PORT}`)
 })
 
-//COPY ROUTE : app.listen 
-app.listen(process.env.PORT || PORT, () => {
-    console.log(`Sever is running on port ${PORT}`)
-})
 
 
 
